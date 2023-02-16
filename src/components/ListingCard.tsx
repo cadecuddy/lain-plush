737323c3e3704fd4b7046f8950bb317c039d7586
Cade Cuddy <cadecuddy@gmail.com>
2023-02-16 17:56:17 -0500

    Add temp border and remove title length restriction

--- src/components/ListingCard.tsx
+++ src/components/ListingCard.tsx
@@ -4,28 +4,22 @@ import Image from "next/image";
 
 export default function ListingCard(listing: LainPlush) {
   return (
-    <div className="flex">
+    // temp border decision TODO: CHANGE
+    <div className="flex border-2 border-slate-500 bg-background p-4">
       <Link href={listing.url} target="_blank">
         <Image
           src={listing.image}
           alt={listing.title}
           width={100}
           height={150}
-          className="h-[150px] w-[100px] rounded-sm shadow-sm transition-all duration-300 hover:scale-105 hover:cursor-pointer"
+          className="h-[150px] w-[150px] rounded-sm shadow-sm transition-all duration-300 hover:scale-105 hover:cursor-pointer md:h-[150px] md:w-[125px]"
         />
       </Link>
       <div className="ml-3 flex-col items-center">
         <Link href={listing.url} target="_blank">
-          <h1 className="px-1 text-lg font-thin hover:cursor-pointer hover:bg-neutral-300 hover:text-background">
-            [
-            {listing.title.length > 50
-              ? listing.title.slice(0, 50) + "..."
-              : listing.title}
-            ]
+          <h1 className="px-1 text-base font-thin hover:cursor-pointer hover:bg-neutral-300 hover:text-background md:text-lg lg:text-xl">
+            [{listing.title}]
           </h1>
-          <h2 className="inline-block px-1 text-base text-green-700 hover:bg-green-700 hover:text-background">
-            Sold {new Date(listing.endTime * 1000).toLocaleDateString()}
-          </h2>
           {listing.endTime < Date.now() / 1000 ? (
             <h2 className="inline-block px-1 text-sm text-green-700 hover:bg-green-700 hover:text-background md:text-lg">
               Sold {new Date(listing.endTime * 1000).toLocaleDateString()}

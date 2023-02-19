6d3bb2e9c59f043e5a91902e699bb835becddd42
Cade Cuddy <cadecuddy@gmail.com>
2023-02-19 11:12:01 -0500

    Update vercel config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
-import { type AppType } from "next/app";
+import type { AppProps, AppType } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
 
 export default api.withTRPC(MyApp);
